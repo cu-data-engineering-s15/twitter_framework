90b696a9a451a312ce60131e4d84543383827e8e
Louis BOUDDHOU <louis.bouddhou@gmail.com>
2015-03-01 20:58:56 -0700

    Adding comments

--- apps/get_friendships_lookup.rb
+++ apps/get_friendships_lookup.rb
@@ -7,14 +7,21 @@ get_friendships_lookup = Returns the relationships of a given user to the comma-
 list of up to 100 screen_names or user_ids provided. 
 
 USAGE =
+  ruby get_friendships_lookup.rb <options> <screen_names> 
+
+  <screen_name>: comma separated list of Screen Names.
+
+  The following options are supported:
 }
 
 def parse_command_line
-
+  
+  # Create ruby hash representing JSON schema
   options = {type: :string, required: true}
 
+  # trollop returns a hash of the command line required strings
   opts = Trollop::options do
-    version "get_list_members 0.1 (c) 2015 Kenneth M. Anderson; Updated by David Aragon"
+    version "get_friendships_lookup 0.1 (c) 2015 Kenneth M. Anderson; Updated by David Aragon"
     banner USAGE
     opt :props, "OAuth Properties File", options
   end	
@@ -28,8 +35,15 @@ def parse_command_line
 
 end
 
-if __FILE__ = $0
+# Only run the following code when this file ("__FILE__") is the main file 
+# being run ("$0") instead of having been required or loaded by another 
+# file; "$:" is the LOAD_PATH directory
 
+if __FILE__ = $0
+  # Find the parent directory of this file and add it to the front
+  # of the list of locations to look in when using require
+
+  # synchronise STDOUT by default to see output as it happens.
   STDOUT.sync = true
 
   input  = parse_command_line
