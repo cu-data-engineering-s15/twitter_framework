9e1bafc0ec56bbd6a07a39a294b2604f2f2fed89
Louis BOUDDHOU <louis.bouddhou@gmail.com>
2015-03-01 17:18:43 -0700

    Getting rid of placeholders in get_friendships_lookup.rb

--- apps/get_friendships_lookup.rb
+++ apps/get_friendships_lookup.rb
@@ -10,9 +10,45 @@ USAGE =
 }
 
 def parse_command_line
-	render :nothing => true
+
+  options = {type: :string, required: true}
+
+  opts = Trollop::options do
+    version "get_list_members 0.1 (c) 2015 Kenneth M. Anderson; Updated by David Aragon"
+    banner USAGE
+    opt :props, "OAuth Properties File", options
+  end	
+		
+  unless File.exist?(opts[:props])
+    Trollop::die :props, "must point to a valid oauth properties file"
+  end
+
+  opts[:screen_name] = ARGV[0]
+  opts
+
 end
 
 if __FILE__ = $0
-	render :nothing => true
+
+  STDOUT.sync = true
+
+  input  = parse_command_line
+  params = { screen_name: input[:screen_name] }
+  data   = { props: input[:props] }
+
+  args     = { params: params, data: data }
+
+  twitter = FriendshipLookup.new(args)
+
+  puts "Collecting up to 100 friendships of '#{input[:screen_name]}'"
+
+  File.open('friendships.json' , 'w') do |f|
+    twitter.collect do |friendships|
+      tweets.each do |friendship|
+        f.puts "#{friendship.to_json}\n"
+      end
+    end
+  end
+
+  puts "DONE."	
 end
