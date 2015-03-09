4847ae1ff60e874b21e02c22906516ede48acac0
nathan-lapinski <n4t311@gmail.com>
2015-03-08 19:28:01 -0700

    New changes

---
+++ apps/get_list_members_show.rb
@@ -0,0 +1,59 @@
+require_relative '../requests/ListMembersShow'
+
+require 'trollop'
+
+USAGE = %Q{
+get_list_members_show: Check if the specified user is a member of the specified list.
+
+Usage:
+  ruby get_list_members.rb <options> <owner_screen_name> <list_slug>
+
+  <owner_screen_name>: The screen name of the list owner.
+  <list_slug>: The slug of the list.
+}
+
+def parse_command_line
+
+  options = {type: :string, required: true}
+
+  opts = Trollop::options do
+    version "get_list_members_show 0.1 (c) 2015 Kenneth M. Anderson; Updated by Nathan Lapinski"
+    banner USAGE
+    opt :props, "OAuth Properties File", options
+  end
+
+  unless File.exist?(opts[:props])
+    Trollop::die :props, "must point to a valid oauth properties file"
+  end
+
+  opts[:owner_screen_name] = ARGV[0]
+  opts[:list_slug] = ARGV[1]
+  opts[:screen_name] = ARGV[2]
+  opts
+end
+
+if __FILE__ == $0
+
+  STDOUT.sync = true
+
+  input  = parse_command_line
+  params = { slug: input[:list_slug], owner_screen_name: input[:owner_screen_name], screen_name: input[:screen_name] }
+  data   = { props: input[:props] }
+
+  args     = { params: params, data: data }
+
+  twitter = ListMembersShow.new(args)
+
+  puts "Collecting the ids of the Twitter users that are members of list '#{input[:list_slug]}'"
+
+  File.open('get_list_members_show.txt', 'w') do |f|
+    twitter.collect do |ids|
+      ids.each do |id|
+        f.puts "#{id}\n"
+      end
+    end
+  end
+
+  puts "DONE."
+
+end
---
+++ requests/ListMembersShow.rb
@@ -0,0 +1,39 @@
+require_relative '../core/TwitterRequest'
+
+class ListMembersShow < TwitterRequest
+
+  def initialize(args)
+    super args
+    #params[:count] = 1000
+    @count = 0
+  end
+
+  def request_name
+    "ListMembersShow"
+  end
+
+  def twitter_endpoint
+    "/lists/members/show"
+  end
+
+  def url
+    'https://api.twitter.com/1.1/lists/members/show.json'
+  end
+
+  def success(response)
+    lists = JSON.parse(response.body)['lists']
+    @count += lists.size
+    log.info("#{lists.size} list(s) received.")
+    log.info("#{@count} total list(s) received.")
+    yield lists
+  end
+
+  def error(response)
+    if response.code == 404
+      puts "No information found at specified url"
+      return
+    end
+    super
+  end
+
+end
